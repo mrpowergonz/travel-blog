4cb400ff1e5b9bcc110a62fdaadb89a4f11a8160
Adam <adampg74@gmail.com>
2024-02-13 16:50:39 +0100

    Tarjetas de enlaces

--- app/app.py
+++ app/app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/about')
+@app.route('/consejos')
 def about():
-    return render_template('about.html')
+    return render_template('consejos.html')
 
-@app.route('/blog')
+@app.route('/empleos')
 def blog():
-    return render_template('blog.html')
+    return render_template('empleos.html')
 
-@app.route('/contact')
+@app.route('/recursos')
 def contact():
-    return render_template('contact.html')
+    return render_template('recursos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/static/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let slideIndex = 0;
+    let showSlidesTimeout;
+
+    showSlides();
+    document.getElementById('prevSlideBtn').addEventListener('click', function() {
+        prevSlide();
+    });
+
+    document.getElementById('nextSlideBtn').addEventListener('click', function() {
+        nextSlide();
+    });
+
+    function showSlides() {
+        let slides = document.querySelectorAll('.slide');
+        if (slideIndex >= slides.length) { slideIndex = 0 }
+        if (slideIndex < 0) { slideIndex = slides.length - 1 }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+        slides[slideIndex].style.display = 'flex';
+        slides[slideIndex].classList.add('center-items'); // Add class to center items
+        slideIndex++;
+
+        // Clear existing timeout before starting a new one
+        clearTimeout(showSlidesTimeout);
+        showSlidesTimeout = setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
+
+    function prevSlide() {
+        slideIndex -= 2;
+        showSlides();
+    }
+
+    function nextSlide() {
+        showSlides();
+    }
+});
